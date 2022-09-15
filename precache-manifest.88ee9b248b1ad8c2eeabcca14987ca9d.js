c70c0ccb76caed8bdc059e1026544fa00635f053
Vikas Yadav <vkyadav.del@gmail.com>
2022-09-15 20:51:13 +0530

    Updates

---
+++ precache-manifest.88ee9b248b1ad8c2eeabcca14987ca9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a908d170066aee7f8ea",
+    "url": "/changer-app/static/css/main.8d25c18e.chunk.css"
+  },
+  {
+    "revision": "0a908d170066aee7f8ea",
+    "url": "/changer-app/static/js/main.0a908d17.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/changer-app/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/changer-app/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "f85fb179352333696d25",
+    "url": "/changer-app/static/js/runtime~main.f85fb179.js"
+  },
+  {
+    "revision": "73ce9f4ef70c6d8b20ad7224d4a2c8c5",
+    "url": "/changer-app/index.html"
+  }
+];
